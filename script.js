d656a1504dfc768364262ec6f1de6125f9e585a3
Pavel <kurilo.pavel@mail.ru>
2024-04-30 15:32:41 +0300

    main page

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-const handleClick = (e)=>{
-  console.log(e)
-}
+const handleClick = (e) => {
+  const children = e.parentElement.children;
+  for (const page of children) {
+    page.className = "page";
+  }
+  e.className = "page page_active";
+};
+
+const scrollStart = () => {
+  window.scroll(0, 0);
+};
